6369cad855d291f8fd490b96e1e1fc62055a7ed4
Mark Leone <mark.leone@axiosengineering.com>
2014-06-08 01:40:10 -0400

    Re-factor to use combination of persistent callbacks and promises. Add ability to cancel callbacks and promises

--- app/js/client/client.js
+++ app/js/client/client.js
@@ -3,7 +3,7 @@ var ozpIwc=ozpIwc || {};
 /**
  * @class
  * This class will be heavily modified in the future.
- * 
+ *
  * @todo accept a list of peer URLs that are searched in order of preference
  * @param {object} config
  * @param {string} config.peerUrl - Base URL of the peer server
@@ -15,6 +15,8 @@ ozpIwc.Client=function(config) {
 	this.peerUrl=config.peerUrl;
 	var a=document.createElement("a");
 	a.href = this.peerUrl;
+	this.replyCallbacks={};
+    this.promises={};
 	this.peerOrigin=a.protocol + "//" + a.hostname;
 	if(a.port)
 		this.peerOrigin+= ":" + a.port;
@@ -59,14 +61,16 @@ ozpIwc.Client=function(config) {
  * @returns {undefined}
  */
 ozpIwc.Client.prototype.receive=function(packet) {
-	if(packet.replyTo) {
-        var replyEvent = {};
-        replyEvent.msgId = packet.replyTo;
-        replyEvent.packet = packet;
-        this.events.trigger("reply", replyEvent);
-	} else {
-		this.events.trigger("receive",packet);
-	}	
+    if(packet.replyTo) {
+        if (this.replyCallbacks[packet.replyTo]) {
+            this.replyCallbacks[packet.replyTo](packet);
+        }
+        if (this.promises[packet.replyTo]) {
+            this.promises[packet.replyTo].setReply(packet);
+        }
+    } else {
+        this.events.trigger("receive",packet);
+    }
 };
 /**
  * Used to send a packet
@@ -74,115 +78,114 @@ ozpIwc.Client.prototype.receive=function(packet) {
  * @param {object} entity - payload of the packet
  * @param {function} callback - callback for any replies
  */
-ozpIwc.Client.prototype.send=function(fields,replyHandlerConfig) {
-	var now=new Date().getTime();
-	var id="p:"+this.msgIdSequence++; // makes the code below read better
+ozpIwc.Client.prototype.send=function(fields,config) {
+    var now=new Date().getTime();
+    var id="p:"+this.msgIdSequence++; // makes the code below read better
 
-	var packet={
-		ver: 1,
-		src: this.participantId,
-		msgId: id,
-		time: now
-	};
+    var packet={
+        ver: 1,
+        src: this.participantId,
+        msgId: id,
+        time: now
+    };
 
-	for(var k in fields) {
-		packet[k]=fields[k];
-	}
+    for(var k in fields) {
+        packet[k]=fields[k];
+    }
 
     var retVal={};
-    if(replyHandlerConfig) {
-        promise = this.makePromise(id,replyHandlerConfig.persist);
+    if (config && config.oneTime) {
+        var promise = this.makePromise(id);
+        this.promises[id]=promise;
         retVal.promise = promise;
+    } else if (config && config.callback) {
+        this.replyCallbacks[id]=config.callback;
     }
-	var data=JSON.stringify(packet);
-	this.peer.postMessage(data,'*');
-	this.sentBytes+=data.length;
-	this.sentPackets++;
+    var data=JSON.stringify(packet);
+    this.peer.postMessage(data,'*');
+    this.sentBytes+=data.length;
+    this.sentPackets++;
     retVal.packet=packet;
-    console.log("send: id: " + id + " action: " + packet.action);
-	return retVal;
+    retVal.msgId=id;
+    return retVal;
 };
 
-ozpIwc.Client.prototype.makePromise=function(msgId,persistHandler) {
-    var self = this;
-    return new Promise(function(resolve,reject) {
-        self.events.on("reply",function(event) {
-            if (event.msgId === msgId) {
-                //BEGIN TEMP CODE
-                console.log("reply to ID " + msgId);
-                //END TEMP CODE
-                var retVal={};
-                if (persistHandler) {
-                    newPromise=self.makePromise(msgId,persistHandler);
-                    retVal.promise=newPromise;
-                }
-                retVal.packet=event.packet;
-                resolve(retVal);
-            }
-        });
-        self.events.on("cancelReply",function(event) {
-            if (event.msgId === msgId) {
-                reject(Error(event.msgId));
-            }
-        });
+ozpIwc.Client.prototype.makePromise=function(msgId) {
+    var resolveCB=null;
+    var promise=new Promise(function(resolve,reject) {
+      resolveCB=resolve;
     });
-}
+    promise.setReply=function(packet) {
+        resolveCB(packet);
+    }
+    return promise;
+};
+
+ozpIwc.Client.prototype.cancelCallback=function(msgId,oneTime) {
+    if (oneTime) {
+        this.promises[msgId].setReply();
+        this.promises[msgId]=undefined;
+    } else {
+        this.replyCallbacks[msgId]=undefined;
+    }
+};
+
 
 ozpIwc.Client.prototype.on=function(event,callback) {
-	if(event==="connected" && this.participantId !=="$nobody") {
-		callback(this);
-		return;
-	}
-	return this.events.on.apply(this.events,arguments);
+    if(event==="connected" && this.participantId !=="$nobody") {
+        callback(this);
+        return;
+    }
+    return this.events.on.apply(this.events,arguments);
 };
 
-ozpIwc.Client.prototype.off=function(event,callback) { 
-	return this.events.off.apply(this.events,arguments);
+ozpIwc.Client.prototype.off=function(event,callback) {
+    return this.events.off.apply(this.events,arguments);
 };
 
 ozpIwc.Client.prototype.disconnect=function() {
-	window.removeEventListener("message",this.messageEventListener,false);
-	if(this.iframe) {
-		document.body.removeChild(this.iframe);
-	}
+    window.removeEventListener("message",this.messageEventListener,false);
+    if(this.iframe) {
+        document.body.removeChild(this.iframe);
+    }
 };
 
 ozpIwc.Client.prototype.createIframePeer=function(peerUrl) {
-	var self=this;
-	var createIframeShim=function() {
-		self.iframe=document.createElement("iframe");
-		self.iframe.src=peerUrl+"/iframe_peer.html";
-		self.iframe.height=1;
-		self.iframe.width=1;
-		self.iframe.style="display:none !important;";
-		self.iframe.addEventListener("load",function() { self.requestAddress(); },false);	
-		document.body.appendChild(self.iframe);
-		self.peer=self.iframe.contentWindow;
+    var self=this;
+    var createIframeShim=function() {
+        self.iframe=document.createElement("iframe");
+        self.iframe.src=peerUrl+"/iframe_peer.html";
+        self.iframe.height=1;
+        self.iframe.width=1;
+        self.iframe.style="display:none !important;";
+        self.iframe.addEventListener("load",function() { self.requestAddress(); },false);
+        document.body.appendChild(self.iframe);
+        self.peer=self.iframe.contentWindow;
 
-	};
-	// need at least the body tag to be loaded, so wait until it's loaded
-	if(document.readyState === 'complete' ) {
-		createIframeShim();
-	} else {
-		window.addEventListener("load",createIframeShim,false);
-}
+    };
+    // need at least the body tag to be loaded, so wait until it's loaded
+    if(document.readyState === 'complete' ) {
+        createIframeShim();
+    } else {
+        window.addEventListener("load",createIframeShim,false);
+    }
 };
 
 ozpIwc.Client.prototype.findPeer=function() {
-	// check if we have a parent, get address there if so
+    // check if we have a parent, get address there if so
 //	if(window.parent!==window) {
 //		this.peer=window.parent;
 //		this.requestAddress();
 //	} else {
-		this.createIframePeer(this.peerUrl);
+    this.createIframePeer(this.peerUrl);
 //	}
 };
 
 ozpIwc.Client.prototype.requestAddress=function(){
-	// send connect to get our address
-	var self=this;
-	this.send({dst:"$transport"}, true).promise.then(function(reply) {
-		self.participantId=reply.dst;
-		self.events.trigger("connected",self);
-	});
+    // send connect to get our address
+    var self=this;
+    this.send({dst:"$transport"}, {oneTime:true}).promise.then(function(reply) {
+        self.participantId=reply.dst;
+        self.events.trigger("connected",self);
+    });
 };
--- demo/bouncingBalls/js/bouncingBalls.js
+++ demo/bouncingBalls/js/bouncingBalls.js
@@ -2,173 +2,164 @@
 var balls={};
 
 var Ball=function(ballRef,svgElement) {
-	this.svg=svgElement;
-	this.el=document.createElementNS("http://www.w3.org/2000/svg", 'g');
-	this.el.setAttribute("class","ball");
+    this.svg=svgElement;
+    this.el=document.createElementNS("http://www.w3.org/2000/svg", 'g');
+    this.el.setAttribute("class","ball");
 
-	this.circle=document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-	this.el.appendChild(this.circle);
-	
-	this.label=document.createElementNS("http://www.w3.org/2000/svg", 'text');
-	this.label.setAttribute("class","svgHidden");
-	this.el.appendChild(this.label);
-	
-	this.svg.append(this.el);
+    this.circle=document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    this.el.appendChild(this.circle);
 
-	
-	this.ballResource=ballRef;
-	this.totalLatency=0;
-	this.packets=0;
-	this.lastUpdate=ozpIwc.util.now();
-	this.updateDelta=0;
-	this.updateCount=0;
-	
-	var watchRequest={
-		dst: "keyValue.api",
-		action: "watch",
-		resource: ballRef
-	};
-	var self=this;
-    var retVal = client.send(watchRequest,{persist:true});
+    this.label=document.createElementNS("http://www.w3.org/2000/svg", 'text');
+    this.label.setAttribute("class","svgHidden");
+    this.el.appendChild(this.label);
+
+    this.svg.append(this.el);
+
+
+    this.ballResource=ballRef;
+    this.totalLatency=0;
+    this.packets=0;
+    this.lastUpdate=ozpIwc.util.now();
+    this.updateDelta=0;
+    this.updateCount=0;
+
+    var watchRequest={
+        dst: "keyValue.api",
+        action: "watch",
+        resource: ballRef
+    };
+    var self=this;
+    var retVal=client.send(watchRequest,{callback:function(packet) {
+        self.packets++;
+        var now=ozpIwc.util.now();
+        self.totalLatency+=now-packet.time;
+
+        if(packet.action==="changed") {
+            self.draw(packet.entity.newValue);
+        }
+    }});
     var packet = retVal.packet;
-	retVal.promise.then(function(reply) {
-        self.drawNew(reply.packet,reply.promise);
-	}).catch(function(msgId) {
-        console.log("Reply for msgId " + msgId + " canceled");
+    //Test cancel of callback
+    //setTimeout(function() {
+    //    client.cancelCallback(retVal.msgId);
+    //},10000);
+
+    $(this.el).click(function() {
+        if(self.label.getAttribute("class").match("svgHidden")) {
+            self.label.setAttribute("class","");
+        }else {
+            self.label.setAttribute("class","svgHidden");
+        }
     });
-	
-	$(this.el).click(function() {
-		if(self.label.getAttribute("class").match("svgHidden")) {
-			self.label.setAttribute("class","");
-		}else {
-			self.label.setAttribute("class","svgHidden");
-		}
-	});	
-	this.watchId=packet.msgId;	
+    this.watchId=packet.msgId;
 };
 
-Ball.prototype.drawNew=function(packet,promise) {
-    var self=this;
-    self.packets++;
-    var now=ozpIwc.util.now();
-    self.totalLatency+=now-packet.time;
-
-    if(packet.action==="changed") {
-        self.draw(packet.entity.newValue);
-    }
-    if (promise) {
-        promise.then(function(reply) {
-            self.drawNew(reply.packet,reply.promise);
-        });
-    }
-}
-
 Ball.prototype.draw=function(info) {
-	if(!info) {
-		this.remove();
-	}
-	var now=ozpIwc.util.now();
-	this.updateDelta+=now-this.lastUpdate;
-	this.updateCount++;
-	this.lastUpdate=now;
-	
-	this.el.setAttribute("transform","translate(" + info.x +","+ info.y + ")");
+    if(!info) {
+        this.remove();
+    }
+    var now=ozpIwc.util.now();
+    this.updateDelta+=now-this.lastUpdate;
+    this.updateCount++;
+    this.lastUpdate=now;
+
+    this.el.setAttribute("transform","translate(" + info.x +","+ info.y + ")");
 //	this.el.setAttribute("y",info.y);
-	this.circle.setAttribute("r",info.r);
-  this.circle.setAttribute("fill",info.color);
-	this.label.setAttribute("x",info.r  + 5);
-	this.label.textContent=info.label
-			+ "[pkt=" + this.packets 
-			+ ",updateAvg=" + Math.floor(this.updateDelta/this.updateCount) + "ms"
+    this.circle.setAttribute("r",info.r);
+    this.circle.setAttribute("fill",info.color);
+    this.label.setAttribute("x",info.r  + 5);
+    this.label.textContent=info.label
+        + "[pkt=" + this.packets
+        + ",updateAvg=" + Math.floor(this.updateDelta/this.updateCount) + "ms"
 //			+ ",avg=" + (this.totalLatency/this.packets).toPrecision(2)
-			+']';
-			
+        +']';
+
 };
 
 Ball.prototype.remove=function() {
-	client.send({
-		dst: "keyValue.api",
-		action: "unwatch",
-		replyTo: this.watchId
-	});
-	this.el.remove();
-	delete balls[this.ballResource];
+    client.send({
+        dst: "keyValue.api",
+        action: "unwatch",
+        replyTo: this.watchId
+    });
+    this.el.remove();
+    delete balls[this.ballResource];
 };
 
 
 var extents={
-	minX: 0,
-	minY: 0,
-	maxX: 1000,
-	maxY: 1000
+    minX: 0,
+    minY: 0,
+    maxX: 1000,
+    maxY: 1000
 };
 
 var AnimatedBall=function(config) {
-	config = config || {};
-	this.state={
-		x: 100+Math.floor(Math.random()*100),
-		y: 100+Math.floor(Math.random()*100),
-		vx: -100+Math.floor(Math.random()*200),
-		vy: -100+Math.floor(Math.random()*200),
-		r: 25+Math.floor(Math.random()*50),
-		color: currentColor,
-		owner: config.owner,
-		label: config.resource
-	};
-	this.resource=config.resource;
-	this.onTick=config.onTick || function() {};
-	
-	this.tick=function(delta) {
-		var ball=this.state;
-		ball.x+=delta*ball.vx;
-		ball.y+=delta*ball.vy;
+    config = config || {};
+    this.state={
+        x: 100+Math.floor(Math.random()*100),
+        y: 100+Math.floor(Math.random()*100),
+        vx: -100+Math.floor(Math.random()*200),
+        vy: -100+Math.floor(Math.random()*200),
+        r: 25+Math.floor(Math.random()*50),
+        color: currentColor,
+        owner: config.owner,
+        label: config.resource
+    };
+    this.resource=config.resource;
+    this.onTick=config.onTick || function() {};
 
-		if(ball.x-ball.r <= extents.minX || ball.x+ball.r >= extents.maxX) {
-			ball.vx=-ball.vx;
-		}
-		if(ball.y-ball.r <= extents.minY || ball.y+ball.r >= extents.maxY) {
-			ball.vy=-ball.vy;
-		}
-		client.send({
-			dst: "keyValue.api",
-			action: "set",
-			resource: this.resource,
-			entity: ball
-		});
-	};
+    this.tick=function(delta) {
+        var ball=this.state;
+        ball.x+=delta*ball.vx;
+        ball.y+=delta*ball.vy;
 
-	this.cleanup=function() {
-		client.send({
-			dst: "keyValue.api",
-			action: "delete",
-			resource: this.resource
-		});		
-	};
+        if(ball.x-ball.r <= extents.minX || ball.x+ball.r >= extents.maxX) {
+            ball.vx=-ball.vx;
+        }
+        if(ball.y-ball.r <= extents.minY || ball.y+ball.r >= extents.maxY) {
+            ball.vy=-ball.vy;
+        }
+        client.send({
+            dst: "keyValue.api",
+            action: "set",
+            resource: this.resource,
+            entity: ball
+        });
+    };
+
+    this.cleanup=function() {
+        client.send({
+            dst: "keyValue.api",
+            action: "delete",
+            resource: this.resource
+        });
+    };
 };
 
 var ourBalls=[];
 var currentColor="black";
 
 $(document).ready(function(){
-	var colors=$("#ballColor option");
-	currentColor=colors[Math.floor(Math.random() * colors.length)].value;
-	$("#ballColor").val(currentColor);
-	
-	var setBallsColor=function(color) {
-		$('#viewport rect')[0].setAttribute("fill",color);
-		for(var i=0;i<ourBalls.length;++i) {
-			ourBalls[i].state.color=color;
-		}
-	};
-	
-	setBallsColor(currentColor);
-	
-	$("#ballColor").change(function(e) {
-		var color=e.target.value;
-		setBallsColor(color);
-	});
+    var colors=$("#ballColor option");
+    currentColor=colors[Math.floor(Math.random() * colors.length)].value;
+    $("#ballColor").val(currentColor);
 
-	
+    var setBallsColor=function(color) {
+        $('#viewport rect')[0].setAttribute("fill",color);
+        for(var i=0;i<ourBalls.length;++i) {
+            ourBalls[i].state.color=color;
+        }
+    };
+
+    setBallsColor(currentColor);
+
+    $("#ballColor").change(function(e) {
+        var color=e.target.value;
+        setBallsColor(color);
+    });
+
+
 //	window.setInterval(function() {
 //		var elapsed=(ozpIwc.util.now()-client.startTime)/1000;
 //	
@@ -184,104 +175,89 @@ $(document).ready(function(){
 var client=new ozpIwc.Client({peerUrl:"http://" + window.location.hostname + ":13000"});
 
 client.on("connected",function() {
-	// setup
-	var viewPort=$('#viewport');
+    // setup
+    var viewPort=$('#viewport');
 
-	$('#myAddress').text(client.participantId);
-	
-	//=================================================================
-	// cleanup when we are done
-	window.addEventListener("beforeunload",function() {
-		for(var i=0;i<ourBalls.length;++i) {
-			ourBalls[i].cleanup();
-		}
-	});
+    $('#myAddress').text(client.participantId);
 
-	//=================================================================
-	// Animate our balls
-	var lastUpdate=new Date().getTime();
-	var animate=function() {
-		var now=new Date().getTime();
-		var delta=(now-lastUpdate)/1000.0;
-		for(var i=0;i<ourBalls.length;++i) {
-			ourBalls[i].tick(delta);
-		}
-		lastUpdate=now;
-	};
-	
-	window.setInterval(animate,50);
-
-	
-	//=================================================================
-	// listen for balls changing
-	var watchRequest={
-		dst: "keyValue.api",
-		action: "watch",
-		resource: "/balls"
-	};
-	var onBallsChanged=function(packet,promise) {
-        //BEGIN TEMP CODE
-        console.log("onBallsChanged invoked");
-        //END TEMP CODE
-		if(packet.action!=="changed") {
-            if (promise) {
-                promise.then(function(reply) {
-                    onBallsChanged(reply.packet, reply.promise);
-                });
-            }
-			return;
-		}
-		if(packet.entity.addChild) {
-			balls[packet.entity.addChild]=new Ball(packet.entity.addChild,viewPort);
-		}
-		if(packet.entity.removeChild) {
-			balls[packet.entity.removeChild].cleanup();
-		}
-        if (promise) {
-            promise.then(function(reply) {
-                onBallsChanged(reply.packet, reply.promise);
-            });
+    //=================================================================
+    // cleanup when we are done
+    window.addEventListener("beforeunload",function() {
+        for(var i=0;i<ourBalls.length;++i) {
+            ourBalls[i].cleanup();
         }
-	};
-	client.send(watchRequest,{persist:true}).promise.then(function(reply) {
-        onBallsChanged(reply.packet,reply.promise);
-    }).catch(function(msgId) {
-        //BEGIN TEMP CODE
-        console.log("Reply for mdgId " + msgId + " canceled");
-        //END TEMP CODE
     });
 
-	//=================================================================
-	// get the existing balls
-	var listExistingBalls={
-		dst: "keyValue.api",
-		action: "list",
-		resource: "/balls"
-	};
-	
-	client.send(listExistingBalls,{persist:false}).promise.then(function(reply) {
-		for(var i=0; i<reply.packet.entity.length;++i) {
-			balls[reply.packet.entity[i]]=new Ball(reply.packet.entity[i],viewPort);
-		}
-	});
+    //=================================================================
+    // Animate our balls
+    var lastUpdate=new Date().getTime();
+    var animate=function() {
+        var now=new Date().getTime();
+        var delta=(now-lastUpdate)/1000.0;
+        for(var i=0;i<ourBalls.length;++i) {
+            ourBalls[i].tick(delta);
+        }
+        lastUpdate=now;
+    };
 
-	//=================================================================
-	// add our ball
-	var pushRequest={
-		dst: "keyValue.api",
-		action: "push",
-		resource: "/balls",
-		entity: {} 
-	};
-	
-	client.send(pushRequest,{persist:false}).promise.then(function(reply) {
-		if(reply.packet.action==="success") {
-			ourBalls.push(new AnimatedBall({
-				resource:reply.packet.entity.resource
-			}));
+    window.setInterval(animate,50);
 
-		} else {
-			console.log("Failed to push our ball: " + JSON.stringify(reply.packet,null,2));
-		}	
-	});
+
+    //=================================================================
+    // listen for balls changing
+    var watchRequest={
+        dst: "keyValue.api",
+        action: "watch",
+        resource: "/balls"
+    };
+    var onBallsChanged=function(packet) {
+        if(packet.action!=="changed") {
+            return;
+        }
+        if(packet.entity.addChild) {
+            balls[packet.entity.addChild]=new Ball(packet.entity.addChild,viewPort);
+        }
+        if(packet.entity.removeChild) {
+            balls[packet.entity.removeChild].cleanup();
+        }
+    };
+    client.send(watchRequest,{callback:onBallsChanged});
+
+    //=================================================================
+    // get the existing balls
+    var listExistingBalls={
+        dst: "keyValue.api",
+        action: "list",
+        resource: "/balls"
+    };
+
+    client.send(listExistingBalls,{oneTime:true}).promise.then(function(packet) {
+        for(var i=0; i<packet.entity.length;++i) {
+            balls[packet.entity[i]]=new Ball(packet.entity[i],viewPort);
+        }
+    }).catch(function(err) {
+        console.log("Reply for msgId " + err + " canceled");
+    });
+
+    //=================================================================
+    // add our ball
+    var pushRequest={
+        dst: "keyValue.api",
+        action: "push",
+        resource: "/balls",
+        entity: {}
+    };
+
+    client.send(pushRequest,{oneTime:true}).promise.then(function(packet) {
+        if(packet.action==="success") {
+            ourBalls.push(new AnimatedBall({
+                resource:packet.entity.resource
+            }));
+
+        } else {
+            console.log("Failed to push our ball: " + JSON.stringify(packet,null,2));
+        }
+    }).catch(function(err) {
+        console.log("Reply for msgId " + err + " canceled");
+    });
 });
