225d24c9cd9b3d74b7fd6fc81a889da8e3052c09
Jason Wagner <jason@nialscorva.net>
2014-06-12 10:06:49 -0400

    (BROKEN) Working on fixing the unit tests

---
+++ app/js/bus/api/data/dataApiValue.js
@@ -0,0 +1,31 @@
+ozpIwc.DataApiValue = ozpIwc.util.extend(ozpIwc.CommonApiValue,function(config) {
+	ozpIwc.CommonApiValue.apply(this,arguments);
+	this.children=[];
+});
+
+ozpIwc.CommonApiValue.prototype.toPacket=function() {
+	var packet=ozpIwc.CommonApiValue.prototype.toPacket.apply(this,arguments);
+	packet.links=packet.links || {};
+	packet.links.children=this.children;
+	return packet;
+};
+
+ozpIwc.DataApiValue.prototype.pushChild=function(child) {
+	this.children.push(child);
+	this.version++;
+};
+
+ozpIwc.DataApiValue.prototype.unshiftChild=function(child) {
+	this.children.unshift(child);
+	this.version++;
+};
+
+ozpIwc.DataApiValue.prototype.popChild=function() {
+	this.version++;
+	return this.children.pop();
+};
+
+ozpIwc.DataApiValue.prototype.shiftChild=function() {
+	this.version++;
+	return this.children.shift();
+};
