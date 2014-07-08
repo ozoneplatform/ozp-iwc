7642d38184e4b6f1d6408930fc6c273ccd6368d8
Jason Wagner <jason@nialscorva.net>
2014-07-08 15:17:57 -0400

    Working on fixing the demos so that they load the widget definitions from the server, allowing them to be run on a single-host demo site as well as the grunt test server.

---
+++ demo/gridster/js/widgetDefs.js
@@ -0,0 +1,32 @@
+var widgetDefs={
+	'Blue Balls' : {
+        'href':"http://"+window.location.hostname + ":15000/?color=blue",
+        'height':2,
+        'width':2
+    },
+	'Red Balls' : {
+        'href':"http://"+window.location.hostname + ":15001/?color=red",
+        'height':2,
+        'width':2
+    },
+	'Green Balls' : {
+        'href':"http://"+window.location.hostname + ":15002/?color=green",
+        'height':2,
+        'width':2
+    },
+	'Black Balls' : { 
+        'href':"http://"+window.location.hostname + ":15003/?color=black",
+        'height':2,
+        'width':2
+    },
+    'Random Balls' : { 
+        'href':"http://"+window.location.hostname + ":15003",
+        'height':2,
+        'width':2
+    },
+	'Debugger' : {
+        'href':"../tools/debugger",
+        'height':5,
+        'width':5
+    }
+};
