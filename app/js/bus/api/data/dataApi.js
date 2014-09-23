376367db15219ecc2719ee8ab1841c37410e74cc
Nish <nishantpatel611@gmail.com>
2014-09-22 23:46:00 -0400

    load from API, fix AJAX

--- app/js/bus/api/data/dataApi.js
+++ app/js/bus/api/data/dataApi.js
@@ -19,7 +19,7 @@
  */
 ozpIwc.DataApi = ozpIwc.util.extend(ozpIwc.CommonApiBase,function(config) {
 	ozpIwc.CommonApiBase.apply(this,arguments);
-    
+
 });
 
 /**
@@ -28,7 +28,7 @@ ozpIwc.DataApi = ozpIwc.util.extend(ozpIwc.CommonApiBase,function(config) {
  * @method loadFromServer
  */
 ozpIwc.DataApi.prototype.loadFromServer=function() {
-    this.loadFromEndpoint("data");
+    this.loadFromEndpoint("https://www.owfgoss.org/ng/dev/mp/api");
 };
 
 /**
@@ -108,7 +108,7 @@ ozpIwc.DataApi.prototype.handleAddchild=function(node,packetContext) {
 	var childNode=this.createChild(node,packetContext);
 
 	node.addChild(childNode.resource);
-	
+
 	packetContext.replyTo({
         'response':'ok',
         'entity' : {
