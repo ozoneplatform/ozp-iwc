9fddc95fe3b4208bcbca41f2af5bd4c80952e09f
Kevin Kelly <kevin.kelly@axiosengineering.com>
2014-09-30 12:26:29 -0400

    router resources are removed from the names.api on disconnection

--- app/js/bus/api/names/namesApi.js
+++ app/js/bus/api/names/namesApi.js
@@ -126,23 +126,25 @@ ozpIwc.NamesApi.prototype.makeValue = function(packet) {
  */
 ozpIwc.NamesApi.prototype.handleEventChannelDisconnectImpl = function (packetContext) {
     console.log(this.participant.name,this.participant.address,packetContext);
-    //First check /address
-    var addrString = "/address/" + packetContext.packet.src;
-    var addrs = this.data["/address"].entity;
 
-    console.log(this.data["/address"],addrString);
-    for(var node in addrs){
-        var curNode = addrs[node];
-        if (this.data[curNode] && this.data[curNode].entity.address === packetContext.packet.src) {
-            console.log(this.participant.name, "Removed address resource", node,
-                "for participant", packetContext.packet.src);
-            delete this.data[curNode];
+
+    if(packetContext.packet.entity.participantType === "routerHeartbeat"){
+        // /router
+        delete this.data[packetContext.packet.entity.address];
+    } else {
+        // /address
+        var addrString = "/address/" + packetContext.packet.src;
+        var addrs = this.data["/address"].entity;
+
+        for(var node in addrs){
+            var curNode = addrs[node];
+            if (this.data[curNode] && this.data[curNode].entity.address === packetContext.packet.src) {
+                delete this.data[curNode];
+            }
         }
     }
-
     for(var node in this.dynamicNodes) {
         var resource = this.dynamicNodes[node];
         this.updateDynamicNode(this.data[resource]);
     }
-    console.log(this.participant.address, this.participant.activeStates.leader, this.participant.name, this.data);
 };
