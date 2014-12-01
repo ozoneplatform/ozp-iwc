665f7e73b1fe37add453d7e49818abec34b22b14
Kevin Kelly <kevin.kelly@axiosengineering.com>
2014-12-01 08:52:52 -0500

    fix(client): client disconnect beforeunload now triggers

--- dist/js/ozpIwc-bus.js
+++ dist/js/ozpIwc-bus.js
@@ -7440,6 +7440,7 @@ ozpIwc.CommonApiValue.prototype.updateContent=function(changedNodes) {
  * @param {ozpIwc.TransportPacket} serverData
  */
 ozpIwc.CommonApiValue.prototype.deserialize=function(serverData) {
+    this.entity = this.entity || {};
     for(var i in serverData.entity){
             this.entity[i] = serverData.entity[i];
     }
