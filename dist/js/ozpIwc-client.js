f7996bc1244f0c6a08ab971a121b77f475aa1869
Kevin Kelly <kevin.kelly@axiosengineering.com>
2014-12-04 16:26:48 -0500

    chore(client):remove commented coded

--- dist/js/ozpIwc-client.js
+++ dist/js/ozpIwc-client.js
@@ -3052,24 +3052,10 @@ ozpIwc.Client=function(config) {
     
     /**
      * A map of available apis and their actions.
-     * @todo pull these from the names.api
      * @property apiMap
      * @type Object
      */
-    this.apiMap={
-//        "data.api" : { 'address': 'data.api',
-//            'actions': ["get","set","delete","watch","unwatch","list","addChild","removeChild"]
-//        },
-//        "system.api" : { 'address': 'system.api',
-//            'actions': ["get","set","delete","watch","unwatch","list","launch"]
-//        },
-//        "names.api" : { 'address': 'names.api',
-//            'actions': ["get","set","delete","watch","unwatch","list"]
-//        },
-//        "intents.api" : { 'address': 'intents.api',
-//            'actions': ["get","set","delete","watch","unwatch","list","register","invoke","broadcast"]
-//        }
-    };
+    this.apiMap={};
 
     /**
      * @property wrapperMap
