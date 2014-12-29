d110433ee6fd1f9d1179a906d60d82737902d89e
Kevin Kelly <kevin.kelly@axiosengineering.com>
2014-12-29 15:59:35 -0500

    feat(debugger): Added election timeline tab

--- app/js/debugger/debugger.js
+++ app/js/debugger/debugger.js
@@ -143,3 +143,160 @@ debuggerModule.controller("metricsController",['$scope','$interval',function(sco
     scope.$watch('updateActive', updateTimer);
     scope.$watch('updateFrequency', updateTimer);
 }]);
+
+debuggerModule.controller('electionController',['$scope',function($scope){
+    $scope.onElectionSelect = function(election){
+        $scope.selectedElection = election;
+    };
+
+    $scope.apis = [
+        {
+            value: 'data.api',
+            title: 'Data Api',
+            elections: new vis.DataSet(),
+            electionGroups: new vis.DataSet(),
+            packets: {}
+        },
+        {
+            value: 'names.api',
+            title: 'Names Api',
+            elections: new vis.DataSet(),
+            electionGroups: new vis.DataSet(),
+            packets: {}
+        },
+        {
+            value: 'system.api',
+            title: 'System Api',
+            elections: new vis.DataSet(),
+            electionGroups: new vis.DataSet(),
+            packets: {}
+        },
+        {
+            value: 'intents.api',
+            title: 'Intents Api',
+            elections: new vis.DataSet(),
+            electionGroups: new vis.DataSet(),
+            packets: {}
+        }
+    ];
+
+    $scope.clear = function(){
+        for(var i in $scope.apis){
+            $scope.apis[i].elections = new vis.DataSet();
+            $scope.apis[i].electionGroups = new vis.DataSet();
+            $scope.apis[i].packets = {};
+        }
+        $scope.selectedElection = null;
+    };
+
+    function logPacket(msg) {
+        var packet=msg.packet.data;
+        var text = packet.src;
+        if(packet.action === 'victory'){
+            text = '<label> VICTORY </label>' + text
+        }
+
+        var timelineData = {
+            id: packet.time,
+            content: text,
+            start: new Date(packet.time),
+            end: new Date(packet.time + 250),
+            group: packet.src
+        };
+
+        var timelineBG = {
+            id: 'BG'+packet.time,
+            content: packet.src +' timer',
+            start: new Date(packet.time),
+            end: new Date(packet.time + 250),
+            type: 'background',
+            subgroup: packet.src
+        };
+
+        var timelineGroup = {
+            id: packet.src,
+            content: packet.src
+        };
+
+        var packetData = {
+            id: packet.time,
+            data: packet
+        };
+        switch (packet.dst) {
+            case "data.api.election":
+                $scope.$apply(function() {
+                    $scope.apis[0].electionGroups.update(timelineGroup);
+                    $scope.apis[0].elections.add(timelineData);
+                    $scope.apis[0].packets[packetData.id] = packetData.data;
+                });
+                break;
+            case "names.api.election":
+                $scope.$apply(function() {
+                    $scope.apis[1].electionGroups.update(timelineGroup);
+                    $scope.apis[1].elections.add(timelineData);
+                    $scope.apis[1].packets[packetData.id] = packetData.data;
+                });
+                break;
+            case "system.api.election":
+                $scope.$apply(function() {
+                    $scope.apis[2].electionGroups.update(timelineGroup);
+                    $scope.apis[2].elections.add(timelineData);
+                    $scope.apis[2].packets[packetData.id] = packetData.data;
+                });
+                break;
+            case "intents.api.election":
+                $scope.$apply(function() {
+                    $scope.apis[3].electionGroups.update(timelineGroup);
+                    $scope.apis[3].elections.add(timelineData);
+                    $scope.apis[3].packets[packetData.id] = packetData.data;
+                });
+                break;
+            default:
+                break;
+        }
+    }
+
+    ozpIwc.defaultPeer.on("receive",logPacket);
+    ozpIwc.defaultPeer.on("send",logPacket);
+
+}]).directive('visTimeline', function() {
+        return {
+            restrict : 'EA',
+            replace : true,
+            scope: {
+                timelineId: '='
+            },
+            link: function($scope,element){
+                $scope.container = element[0];
+                // Create a DataSet with data (enables two way data binding)
+                $scope.data = new vis.DataSet();
+
+                // Configuration for the Timeline
+                $scope.options = {
+                    stack:true,
+                    maxHeight: "900px",
+                    groupOrder: 'content'
+                };
+
+                // Create a Timeline
+                $scope.timeline = new vis.Timeline($scope.container, $scope.data, $scope.options);
+
+                $scope.timeline.on('select', function (properties) {
+                    $scope.$parent.packetContents =
+                        JSON.stringify($scope.$parent.selectedElection.packets[properties.items[0]],null,2);
+
+                });
+            },
+            controller: function($scope){
+                $scope.$watch('$parent.selectedElection',function(newVal){
+                    if(newVal) {
+                        $scope.timeline.setGroups(newVal.electionGroups);
+                        $scope.timeline.setItems(newVal.elections);
+                        $scope.timeline.fit();
+                    } else {
+                        $scope.timeline.clear({items:true,groups:true});
+                    }
+                })
+            }
+        };
+    });
