63eaca6dfa9aa5b0e65f56573ee987145979cdbb
Kevin Kelly <kevin.kelly@axiosengineering.com>
2015-08-14 17:13:31 -0400

    chore(release): 1.0.9

---
+++ dist/js/app/js/defaultWiring.js
@@ -0,0 +1,68 @@
+var ozpIwc=ozpIwc || {};
+ozpIwc.version = "1.0.3";
+ozpIwc.log.threshold = 6;
+ozpIwc.ELECTION_TIMEOUT = 3000;
+ozpIwc.heartBeatFrequency = 1000; // 3 seconds
+ozpIwc.apiRootUrl = ozpIwc.apiRootUrl || "/";
+ozpIwc.policyRootUrl = ozpIwc.policyRootUrl || "/policy";
+ozpIwc.basicAuthUsername= ozpIwc.basicAuthUsername || '';
+ozpIwc.basicAuthPassword= ozpIwc.basicAuthPassword || '';
+ozpIwc.linkRelPrefix = ozpIwc.linkRelPrefix || "ozp";
+
+ozpIwc.intentsChooserUri = "intentsChooser.html";
+
+(function() {
+	var params=ozpIwc.util.parseQueryParams();
+	if(params.log) {
+		try{
+			console.log("Setting log level to ",params.log);
+			ozpIwc.log.setThreshold(ozpIwc.log[params.log.toUpperCase()]);
+		}catch(e) {
+			// just ignore it and leave the default level
+		}
+	}
+})();
+
+ozpIwc._busInit = function(){};
+ozpIwc.authorization = new ozpIwc.policyAuth.PDP({
+    'pip': new ozpIwc.policyAuth.PIP(),
+    'prp': new ozpIwc.policyAuth.PRP(),
+    'setsEndpoint': ozpIwc.policyRootUrl
+});
+var enablePostMessageParticipants = function(){};
+
+if (typeof ozpIwc.enableDefault === "undefined" || ozpIwc.enableDefault) {
+    ozpIwc.initEndpoints(ozpIwc.apiRootUrl || "api");
+    ozpIwc.defaultPeer = new ozpIwc.Peer();
+    ozpIwc.defaultLocalStorageLink = new ozpIwc.KeyBroadcastLocalStorageLink({
+        peer: ozpIwc.defaultPeer
+    });
+
+    ozpIwc.defaultRouter = new ozpIwc.Router({
+        peer: ozpIwc.defaultPeer,
+        heartbeatFrequency: ozpIwc.heartBeatFrequency
+    });
+
+    if (typeof ozpIwc.acceptPostMessageParticipants === "undefined" ||ozpIwc.acceptPostMessageParticipants) {
+        ozpIwc.defaultPostMessageParticipantListener = new ozpIwc.PostMessageParticipantListener({
+            router: ozpIwc.defaultRouter,
+            ready: new Promise(function(resolve){
+                enablePostMessageParticipants = resolve;
+            })
+        });
+    }
+
+    ozpIwc._busInit = function() {
+        if (typeof ozpIwc.runApis === "undefined" || ozpIwc.runApis) {
+            ozpIwc.locksApi = new ozpIwc.LocksApi({'name': "locks.api"});
+            ozpIwc.namesApi = new ozpIwc.NamesApi({'name': "names.api"});
+            ozpIwc.dataApi = new ozpIwc.DataApi({'name': "data.api"});
+            ozpIwc.intentsApi = new ozpIwc.IntentsApi({'name': "intents.api"});
+            ozpIwc.systemApi = new ozpIwc.SystemApi({'name': "system.api"});
+        }
+        enablePostMessageParticipants();
+    };
+}
+
+
+ozpIwc.util.prerender().then(ozpIwc._busInit);
---
+++ dist/js/app/js/metrics/statistics/exponentiallyWeightedMovingAverage.js
@@ -0,0 +1,106 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2012 Mike Ihbe
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Original code owned by Mike Ihbe.  Modifications licensed under same terms.
+ */
+var ozpIwc=ozpIwc || {};
+ozpIwc.metricsStats=ozpIwc.metricsStats || {};
+/**
+ * @submodule metrics.statistics
+ */
+
+/**
+ *
+ * @class metricStats
+ * @namespace ozpIwc
+ */
+
+/**
+ * @property M1_ALPHA
+ * @type {Number}
+ * @default 1 - e^(-5/60)
+ */
+ozpIwc.metricsStats.M1_ALPHA = 1 - Math.exp(-5/60);
+
+/**
+ * @property M5_ALPHA
+ * @type {Number}
+ * @default 1 - e^(-5/60/5)
+ */
+ozpIwc.metricsStats.M5_ALPHA = 1 - Math.exp(-5/60/5);
+
+/**
+ * @property M15_ALPHA
+ * @type {Number}
+ * @default 1 - e^(-5/60/15)
+ */
+ozpIwc.metricsStats.M15_ALPHA = 1 - Math.exp(-5/60/15);
+
+/**
+ *  Exponentially weighted moving average.
+ *  @method ExponentiallyWeightedMovingAverage
+ *  @param {Number} alpha
+ *  @param {Number} interval Time in milliseconds
+ */
+ozpIwc.metricsStats.ExponentiallyWeightedMovingAverage=function(alpha, interval) {
+  this.alpha = alpha;
+  this.interval = interval || 5000;
+  this.currentRate = null;
+  this.uncounted = 0;
+	this.lastTick=ozpIwc.util.now();
+};
+
+/**
+ * @method update
+ * @param n
+ */
+ozpIwc.metricsStats.ExponentiallyWeightedMovingAverage.prototype.update = function(n) {
+  this.uncounted += (n || 1);
+	this.tick();
+};
+
+/**
+ * Update the rate measurements every interval
+ *
+ * @method tick
+ */
+ozpIwc.metricsStats.ExponentiallyWeightedMovingAverage.prototype.tick = function() {
+ 	var now=ozpIwc.util.now();
+	var age=now-this.lastTick;
+	if(age > this.interval) {
+		this.lastTick=now - (age % this.interval);
+		var requiredTicks=Math.floor(age / this.interval);
+		for(var i=0; i < requiredTicks; ++i) {
+			var instantRate = this.uncounted / this.interval;
+			this.uncounted = 0;
+			if(this.currentRate!==null) {
+				this.currentRate += this.alpha * (instantRate - this.currentRate);
+			} else {
+				this.currentRate = instantRate;
+			}
+		}
+	}
+};
+
+/**
+ * Return the rate per second
+ *
+ * @returns {Number}
+ */
+ozpIwc.metricsStats.ExponentiallyWeightedMovingAverage.prototype.rate = function() {
+  return this.currentRate * 1000;
+};
