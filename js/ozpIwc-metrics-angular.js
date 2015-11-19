317382f622257f6def4c27e831aaa75675556a73
Kevin Kelly <kevin.kelly@axiosengineering.com>
2015-11-19 09:19:15 -0500

    Updates

--- js/ozpIwc-metrics-angular.js
+++ js/ozpIwc-metrics-angular.js
@@ -1,8 +1,14 @@
 angular.module('ozpIwcMetrics', []).factory('iwcMetrics', function () {
 // If running in a worker, there is no window, rather only self.
 // Reassign window to self in this environment
-if(!window){
-    /*jshint -W020 */
+try {
+    // For IE: Throws error on trying to reassign window
+    if (!window) {
+        /*jshint -W020 */
+        window = self;
+    }
+}catch (e){
+    // For Chrome/FF: window is undefined and throws error on checking if falsy
     window = self;
 }
 /*!
